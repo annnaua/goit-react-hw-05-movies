e12dcf3fb8b21fd5b6b6da5a0d768ab2881f03b8
Anna Riabova <annaua04@gmail.com>
2022-10-28 15:10:57 +0300

    replace folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,7 @@ import { SharedLayout } from './SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('../pages/Home/Home'));
 const Movies = lazy(() => import('../pages/Movies/Movies'));
-const MovieDetails = lazy(() =>
-  import('../components/MovieDetails/MovieDetails')
-);
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
 const Cast = lazy(() => import('../pages/Cast/Cast'));
 const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
 
